a52c884d430d08b2a8c6ceda0f57dd17cec01e2a
Gary Lai <garylai@MacBook-Air-de-Gary.local>
2019-01-25 19:17:46 -0500

    Now allows for user input

--- addLove.js
+++ addLove.js
@@ -1,21 +1,34 @@
-document.onclick = function(){
+document.getElementById("section2").onclick = function(){
     if (idNumber>20)
     {
-            return alert("Too much love!!");
+            return alert("Too much " + text + "!!!!!");
     }
     return addLove();
 }
 
-function addLove () { 
+function initLove(){
+  //initialize
+  // create a new div element 
+  var initDiv = document.createElement("div"); 
+  initDiv.className = "love";
+  initDiv.setAttribute("id", "1");
+  initDiv.setAttribute("style", "position: absolute; top:50%; left:50%; margin:10px;");
+  var initContent = document.createTextNode(text); 
+  initDiv.appendChild(initContent);  
+  document.getElementById("section2").appendChild(initDiv);
+  return;
+}
+
+function addLove() { 
   // create a new div element 
   var newDiv = document.createElement("div"); 
   newDiv.className = "love";
   newDiv.setAttribute("id", idNumber.toString());
-  newDiv.setAttribute("display", "block;");
+  //newDiv.setAttribute("display", "block;");
   //newDiv.setAttribute("margin");
  // console.log("love id is set to:" + idNumber);
   // and give it some content 
-  var newContent = document.createTextNode("LOVE"); 
+  var newContent = document.createTextNode(text); 
   // add the text node to the newly created div
   newDiv.appendChild(newContent);  
 
