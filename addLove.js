67dfb676fc00b1d29bf82a7b2db38469781744ed
Gary Lai <garylai@MacBook-Air-de-Gary.local>
2019-01-26 11:37:03 -0500

    adds screen instructions

--- addLove.js
+++ addLove.js
@@ -1,27 +1,47 @@
+//handle clicks
 document.getElementById("section2").onclick = function(){
-    if (idNumber>20)
+    var instructChange1 = 5;
+    var instructChange2 = 7;
+    var instructChange3 = 9
+    var total = 11;  
+
+    if (idNumber>instructChange1)
+    {
+      document.getElementById("instruction1").innerHTML = "Keep clicking for more " + text + "s!!";   
+    }
+    if (idNumber>instructChange2)
+    {
+      document.getElementById("instruction1").innerHTML = "Keep going, let's see how many " + text + "s you can give!!!";   
+    }
+    if (idNumber>instructChange3)
+    {
+      document.getElementById("instruction1").innerHTML = "DON'T STOP!!!!! MORE " + text.toUpperCase() + "S!!!";   
+    }
+    if (idNumber>total)
     {
       var finalMsg = new SpeechSynthesisUtterance();
-      finalMsg.text = "20" + " " + text + "s given !!!!!"
+      finalMsg.text = "Wow, 20" + " " + text + "s were given, that's a lot of " + text + "s!!!"
       speechSynthesis.speak(finalMsg);
-      return alert("20" + " " + text + "s given !!!!!");
+      return alert("20" + " " + text + "s were given !!! That's a lot of " + text + "!!!");
     }
-   
     return addLove();
 }
 
 function initLove(){
   //clear the field
-  idNumber = 2;
+  idNumber = 1;
   document.getElementById("section2").innerHTML = "";
+
+  
   //initialize
   var initDiv = document.createElement("div"); 
-  initDiv.className = "love";
-  initDiv.setAttribute("id", "1");
-  initDiv.setAttribute("style", "position: absolute; top:50%; left:50%; margin:10px;");
-  var initContent = document.createTextNode(text); 
+  initDiv.className = "instruction";
+  initDiv.setAttribute("style", "position: absolute; left: 40%; top: 90%");
+  initDiv.setAttribute("id", "instruction1");
+  var initContent = document.createTextNode("Click the screen for more " + text + "s!"); 
   initDiv.appendChild(initContent);  
   document.getElementById("section2").appendChild(initDiv);
+
   //window.location.href = '#section2'; //here needs to add animation
   window.scroll({
     top: window.innerHeight,
@@ -40,11 +60,9 @@ function addLove() {
   var newContent = document.createTextNode(text); 
   // add the text node to the newly created div
   newDiv.appendChild(newContent);  
-
   // add the newly created element and its content into the DOM 
   document.getElementById("section2").appendChild(newDiv);
   //play audio
-  //var audioText = document.getElementById('1').innerHTML;
   var audioText = text;
   var msg = new SpeechSynthesisUtterance();
   msg.text = audioText;
